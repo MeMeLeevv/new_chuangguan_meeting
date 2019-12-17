d611d58700ccb6b4ca27c896dce2c2ce03b6ff6b
LemonTency <1045413757@qq.com>
2019-12-17 18:30:49 +0800

    guests Page needs to be fixed

---
+++ js/news.js
@@ -0,0 +1,12 @@
+;(function ($, win, doc) {
+  // 全局共享数据
+  var newsData = {
+    data: {
+      newsScrollId: '#nWrap'
+    },
+    init: function (params) {
+    }
+  } 
+  $(function(){
+  })
+})(jQuery, window, document)
